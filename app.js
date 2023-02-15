3e1843be92334ab2e18d3c5aeb1a94a1de8d2c97
Joshua Zuluaga <zazuofcloud10@gmail.com>
2023-02-14 19:48:31 -0500

    fixed the port so the redux could work with it.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { Post } = require('./models');
 
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 3001;
 
 app.set('view engine', 'ejs');
 app.use(express.json());
